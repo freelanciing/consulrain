bfd8f5351249e459dceba219fdcb2599786cec58
Dell <amerakhames42@gmail.com>
2025-07-16 18:01:52 +0300

    partners

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import OurPartners from "../../components/OurPartners";
 
 export default function HomePage() {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-green-800">
+      {/* <h1 className="text-4xl font-bold text-green-800">
         Welcome to the Home Page
-      </h1>
+      </h1> */}
+      <OurPartners />
     </div>
   );
 }
