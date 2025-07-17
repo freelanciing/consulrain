21dcc2bf806274bfccc6e95e239887b850085da1
omnia <omniaeid314@gmail.com>
2025-07-17 18:00:05 +0300

    hero responsivness

--- src/components/homeComponents/HeroSection.jsx
+++ src/components/homeComponents/HeroSection.jsx
@@ -3,22 +3,25 @@ import { useTranslation } from "react-i18next";
 
 const HeroSection = () => {
   const { t } = useTranslation();
+
   return (
-    <dev
-      className="px-6 lg:px-24 xl:px-16 2xl:px-48 bg-primary-50 bg-cover w-full mt-0 lg:h-auto py-6 lg:py-5 flex lg:flex-nowrap sm:flex-wrap  flex-row sm:flex-col lg:flex-row justify-start gap-6 md:gap-12 lg:gap-36 items-center"
+    <div
+      className="w-full bg-blue-25 bg-cover bg-primary-50 py-4 lg:py-5 flex flex-col lg:flex-row gap-6 md:gap-12 lg:gap-36 items-center lg:px-24 xl:px-16 2xl:px-48"
       style={{
         backgroundImage: "url('/assets/default/img/home/hero-background.jpg')",
       }}
     >
-      <div className="self-center flex gap-2 flex-col 3xl:ms-auto">
-        <p className="text-lg  md:text-2xl lg:text-4xl xl:text-5xl text-pure-turquoise font-bold  lg:text-start mb-2 mt-2 md:mt-0">
+      {/* Left Text Section */}
+      <div className="flex flex-col items-center lg:items-start sm:text-center lg:text-start gap-3 3xl:ms-auto">
+        <p className="text-lg md:text-2xl lg:text-4xl xl:text-5xl text-pure-turquoise font-bold">
           {t("hero.title")}
         </p>
-        <p className="text-base md:text-lg lg:text-xl xl:text-2xl text-sky-950 w-full font-semibold lg:text-start">
+
+        <p className="text-base md:text-lg lg:text-xl xl:text-2xl text-sky-950 font-semibold max-w-3xl">
           {t("hero.subtitle")}
         </p>
 
-        <div className=" mt-2 lg:mt-10 md:mx-auto lg:mx-0 sm:me-auto flex gap-2 flex-wrap justify-start">
+        <div className="mt-4 flex flex-wrap justify-center lg:justify-start md:text-cente gap-2">
           <a
             href="#learningSolutions"
             className="bg-primary-500 text-white px-10 py-3 rounded hover:bg-primary-700 transition font-semibold"
@@ -28,7 +31,11 @@ const HeroSection = () => {
         </div>
       </div>
 
-      <div className="flex flex-col gap-y-2 mb-11 3xl:me-auto">
+      {/* Right Avatar Section */}
+      <div
+        dir="ltr"
+        className="flex flex-col gap-y-2 mb-11 3xl:me-auto justify-center"
+      >
         <div className="flex gap-x-2">
           <div className="relative flex pt-7 w-32 h-32 md:w-48 md:h-48 xl:w-56 xl:h-56 mt-4 rounded-tl-[50%] border-8 border-white bg-pastel-blue">
             <img
@@ -55,15 +62,6 @@ const HeroSection = () => {
 
         <div className="flex gap-x-2">
           <div className="relative flex pt-7 w-32 h-32 md:w-48 md:h-48 xl:w-56 xl:h-56 rounded-bl-[50%] border-8 border-white bg-pastel-green">
-            <div className="hidden md:block absolute top-12 -start-36 w-full h-full">
-              <lottie-player
-                autoplay
-                loop
-                mode="normal"
-                src="https://lottie.host/9a42152a-2ea1-4d79-8c10-a3e4dea063c0/iu7gsKeJqj.json"
-                background="transparent"
-              ></lottie-player>
-            </div>
             <img
               className="mx-auto w-auto h-auto"
               width="180"
@@ -92,7 +90,7 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-    </dev>
+    </div>
   );
 };
 
